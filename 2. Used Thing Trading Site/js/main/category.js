c9704c35079fa39cae8c5f3960053fa824ab445f
Dongwoo-Kim <dongeu47@gmail.com>
2022-01-05 21:03:07 +0900

    220105

---
+++ 2. Used Thing Trading Site/js/main/category.js
@@ -0,0 +1,20 @@
+function categoryEvent(event) {
+  const navCategory = event.currentTarget
+  const categoryDownMenu = document.querySelector('.category-down-menu');
+  const navCategoryIcon = document.querySelector('.category-menu-icon-wrapper > i');
+
+  categoryDownMenu.classList.toggle('category__active');
+
+  if (categoryDownMenu.classList.contains('category__active')) {
+    navCategory.classList.add('nav__category__active');
+    navCategoryIcon.classList.remove('fa-chevron-down');
+    navCategoryIcon.classList.add('fa-chevron-up');
+  } else {
+    navCategory.classList.remove('nav__category__active');
+    navCategoryIcon.classList.remove('fa-chevron-up');
+    navCategoryIcon.classList.add('fa-chevron-down');
+  }
+}
+
+const navCategory = document.querySelector('.nav__category');
+navCategory.addEventListener('click', categoryEvent);
---
+++ 2. Used Thing Trading Site/js/main/slider.js
@@ -0,0 +1,2 @@
+const leftArrow = document.querySelector('.arrow-left');
+const rightArrow = document.querySelector('.arrow-right');
